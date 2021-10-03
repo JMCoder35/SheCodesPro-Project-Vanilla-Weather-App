ce93ecb9cfcb8e89b9df2cb030aeab7d553b9c63
JMCoder35 <89359935+JMCoder35@users.noreply.github.com>
2021-10-03 17:30:35 -0400

    Added day/time JS functions

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursay",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data.main);
   let temperatureElement = document.querySelector("#currentTemp");
@@ -5,11 +28,15 @@ function displayTemperature(response) {
   let weatherDescription = document.querySelector("#weatherDescription");
   let humidityElement = document.querySelector("#humidity");
   let feelsLikeElement = document.querySelector("#feelsLike");
+  let dateTimeElement = document.querySelector("#dateTime");
+  let windSpeedElement = document.querySelector("#windSpeed");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
   feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  dateTimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "29ed711e6c528e8877439d3d2d9efee4";
