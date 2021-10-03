6974caad98307bef3c38dec7a78b27be27406e50
JMCoder35 <89359935+JMCoder35@users.noreply.github.com>
2021-10-03 16:56:16 -0400

    Showing current temperature in JS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data.main);
+  let temperatureElement = document.querySelector("#currentTemp");
+  let cityElement = document.querySelector("#city");
+  let weatherDescription = document.querySelector("#weatherDescription");
+  let humidityElement = document.querySelector("#humidity");
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
 }
 
 let apiKey = "29ed711e6c528e8877439d3d2d9efee4";
