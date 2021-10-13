c59d940dabcfca46443610e2717ccf3659b398f1
JMCoder35 <89359935+JMCoder35@users.noreply.github.com>
2021-10-12 23:39:45 -0400

    Changed forecast units

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
 
   let apiKey = "29ed711e6c528e8877439d3d2d9efee4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
